91aa6385f2276e868d823dd1543ec9592cf3fbd2
Joerg-Christian Boehme <joerg@chaosdorf.de>
2019-01-22 08:06:50 +0100

    update docker versions

--- bin-build/android-develop.sh
+++ bin-build/android-develop.sh
@@ -7,8 +7,18 @@ rm -fr $BUILD_DIR
 
 chmod 755 $ANDROID_HOME/tools/bin/sdkmanager
 $ANDROID_HOME/tools/bin/sdkmanager --list
-
+$ANDROID_HOME/emulator/emulator -list-avds
 $ANDROID_HOME/emulator/emulator -avd android-24-test -no-audio -no-window -dns-server 8.8.8.8 &
 
-$ANDROID_HOME/cmake/3.6.4111459/bin/cmake -H$SRC_DIR -B$BUILD_DIR -G'Android Gradle - Ninja' -DANDROID_ABI=armeabi-v7a -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=$ANDROID_HOME/ndk-bundle/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=android-19 -DCMAKE_MAKE_PROGRAM=$ANDROID_HOME/cmake/3.6.4111459/bin/ninja -DANDROID_STL="c++_static" -DANDROID_CPP_FEATURES="exceptions rtti" -DPOCO_ENABLE_TESTS=ON -DPOCO_ENABLE_LONG_RUNNING_TESTS=OFF -DPOCO_OLD_REDIS_VERSION=ON && \
+$ANDROID_HOME/cmake/3.6.4111459/bin/cmake -H$SRC_DIR -B$BUILD_DIR -G'Android Gradle - Ninja' \
+  -DANDROID_ABI=armeabi-v7a \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_TOOLCHAIN_FILE=$ANDROID_HOME/ndk-bundle/build/cmake/android.toolchain.cmake \
+  -DANDROID_PLATFORM=android-19 \
+  -DCMAKE_MAKE_PROGRAM=$ANDROID_HOME/cmake/3.6.4111459/bin/ninja \
+  -DANDROID_STL="c++_static" \
+  -DANDROID_CPP_FEATURES="exceptions rtti" \
+  -DPOCO_ENABLE_TESTS=ON \
+  -DPOCO_ENABLE_LONG_RUNNING_TESTS=OFF \
+  -DPOCO_OLD_REDIS_VERSION=ON && \
 cmake --build $BUILD_DIR --target all && cd $BUILD_DIR && POCO_BASE=$SRC_DIR ctest --output-on-failure
